e17d5d2f322da9d82930a2df180b557c2f55415a
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-03-14 10:59:05 +0300

    assignment_1 added

--- assignment_1/tasks.py
+++ assignment_1/tasks.py
@@ -1,12 +1,25 @@
 from functools import reduce
+from collections import namedtuple
 from operator import mul
+from typing import Tuple
 
-def find_fold_number(n):
-    # Write your code here
-    raise NotImplementedError
 
-def rle(string):
-    # Write your code here
-    raise NotImplementedError
+def find_fold_number(n: int) -> int:
+    s = str(n)
+    ans = 0
+    while len(s) > 1:
+        s = str(reduce(lambda a, b: int(a) * int(b), s, 1))
+        ans += 1
+    return ans
+
+
+def rle(string: str) -> str:
+    def append(acc: Tuple[str, str, int], sym: str) -> Tuple[str, str, int]:
+        if sym == acc[1]:
+            return acc[0], sym, acc[2] + 1
+        else:
+            return acc[0] + acc[1] + (str(acc[2]) if acc[2] != 1 else ''), sym, 1
+    return (reduce(append, string + '|', ('', '', 0))[0])[1:]
+
 
 test_rle_str = 'ffffbbbbbbbbbbbbbsssssssssssssgggggggggggggggeeeeeeeeeeeellllllllllllllllllooooooooooooffffiiiiiirrrrrrrrrrrrrruuuuuuuummmmmiiiiiiiiiiiiiiiqqqxxxxxxxxxxxxxxxxxzzzzzzwwwwzzzzzzzzzzzzzzzzzzzoooooookkkkkkkkkkiiiiiiiiiiiiiiiizzzzzzzzzzzzzzzzhhhhhhhooooooooooccccccccccccccrrrrrrrkkkxxxxxxxxxxxxxxxxxkkssssssssmcccdddddddddddppppppppppppppnnnnnnnnnnnnnnssssssssssssbbbbbraaaaaasjjjjjjjjjjkkkkkkkkkrrrrrrrrryyyyyyyyyyyyaaaaaaaaaaauuuuuuuuuuuuwwwwwwwwwwgggggggglllllllllvvvvvvvvvvvbbgggggggggggggggqqqqqqlllllllllllllllxxxxxxxxxxaaaaaeeeeeeeeeeeeevvvvvvvvvvvvvdddddddddddpccccddddddddddddddddpppppppppppppppssssssssssssddddddddddddddddddzzzzzzzzzzzzzzzzzzzaaaaaaaaaaaaaaaaaaabfffffffffkkkkkxxxxxwwwwwhhhhhhhhhhhhtttttttttrrrrrrrrrrrrrrrrrroooooooooooooooooooccccccccccccccccppphhhhhhhhhhhhuuuuuurrrrrrrrrrrrrrrrrrryyyyyyyyyyyyyyyyyjjjjjjjjjjjjjjjjjjjjjjjjgggaaaxxxxxxxxxxxlllllllluuuuuuuuuummmmmmmmmmffffffmmmmmmmmmmmmmmmmmmggggggggggggggtttjjjjjjjjjjjjjjjjjjj'
